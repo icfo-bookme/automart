0b91a369787d08d2c987a86187c94e15770a1334
icfo-bookem <nationalambulancebd@gmail.com>
2026-09-02 14:57:56 +0600

    GTM Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,9 +77,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5MHGH622');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body
         className={`antialiased ${rubik.variable} font-sans bg-[#F8F8F8]`}
-      ><AuthProvider>
+      >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5MHGH622"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        <AuthProvider>
           <ReduxProvider>
             <Header />
             <div className="col-span-3 md:hidden flex px-2 pt-1">
